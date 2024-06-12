ba14817d9bf08b3d59cece4f008f23ffc5b4fd4e
qruty <64466788+qrutyy@users.noreply.github.com>
2024-06-12 23:23:23 +0300

    chore: setup Spring Boot app project

---
+++ src/main/java/com/app/studyflow/StudyflowApplication.java
@@ -0,0 +1,13 @@
+package com.app.studyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/studyflow/StudyflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.studyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
